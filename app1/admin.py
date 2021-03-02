e2a144815dc5a665f1a4e35442ff31210e8b620f
Zaid <xaidmetamorphos@gmail.com>
2021-03-02 15:30:18 +0530

    database

--- app1/admin.py
+++ app1/admin.py
@@ -1,3 +1,6 @@
+from app1.models import News, Note
 from django.contrib import admin
 
 # Register your models here.
+admin.site.register(News)
+admin.site.register(Note)
--- app1/models.py
+++ app1/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class News(models.Model):
+    headline = models.CharField(max_length=160,null=False)
+    description = models.TextField(default="No description")
+
+    def __repr__(self):
+        return self.headline
+    
+    def __str__(self):
+        return self.headline
+
+
+class Note(models.Model):
+    title = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.title
+
